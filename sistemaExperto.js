57284640fb2ff911a14077b1ea186dc29c4d6260
BalasteguissonPC <gbg.balas@gmail.com>
2022-04-12 09:30:00 +0200

    archivo sistema experto

---
+++ sistemaExperto.js
@@ -0,0 +1,12 @@
+function prescripcion() {
+    
+
+    //base de hechos
+    let enfermedadPrincipal;
+    let edad;
+    let sexo;
+    let embarazo;
+    let lactancia;
+    let tratamientosActuales;
+    let enfermedadesPrevias;
+}
